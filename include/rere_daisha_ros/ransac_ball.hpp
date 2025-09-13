e0e2301198d6abf7f29496a2e657bf36376379d0
aky <akiyugata271@gmail.com>
2025-09-13 17:39:56 +0900

    add sampring

--- include/rere_daisha_ros/ransac_ball.hpp
+++ include/rere_daisha_ros/ransac_ball.hpp
@@ -1,13 +1,19 @@
+#pragma once
+
 #include <Eigen/Dense>
 #include <vector>
+#include <random>
+#include <numeric>
+#include <algorithm>
 
 class RansacBall
 {
 public:
     RansacBall(float r);
+    std::vector<std::vector<float>> run();
 
 private:
-    std::vector<std::vector<float>> run();
+    std::vector<int> sampring(uint max_val, uint num);
 
     const float ball_r;
 };
--- src/ransac_ball.cpp
+++ src/ransac_ball.cpp
@@ -8,5 +8,20 @@ RansacBall::RansacBall(float r)
 
 std::vector<std::vector<float>> run()
 {
-    
+
+}
+
+std::vector<int> RansacBall::sampring(uint max_val, uint num)
+{
+    if (num > max_val + 1) {
+        return {};
+    }
+    int min_val = 0;
+    std::vector<int> numbers(max_val - min_val + 1);
+    std::iota(numbers.begin(), numbers.end(), min_val);
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(numbers.begin(), numbers.end(), g);
+    std::vector<int> return_numbers(numbers.begin(), numbers.begin() + num);
+    return return_numbers;
 }
