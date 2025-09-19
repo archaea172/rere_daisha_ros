e830d6f7907b7e97dd8f4a2beccf0ac086620679
aky <akiyugata271@gmail.com>
2025-09-20 00:30:21 +0900

    fix bug

--- src/debug.cpp
+++ src/debug.cpp
@@ -9,27 +9,27 @@ std::vector<std::vector<float>> generateCirclePointCloud(float radius, int num_p
 
     // C++11以降のモダンな乱数生成器
     std::random_device rd;
-    std::mt19227 gen(rd());
+    std::mt19937 gen(rd());
     // 平均0, 標準偏差noise_levelの正規分布
     std::normal_distribution<> d(0, noise_level);
 
     // M_PIが未定義の場合の対策
     #ifndef M_PI
-    const double M_PI = 3.14159265358979323846;
+    const float M_PI = 3.14159265358979323846;
     #endif
 
     for (int i = 0; i < num_points; ++i) {
         // 0から2πまでの角度を計算
-        double theta = 2.0 * M_PI * static_cast<double>(i) / static_cast<double>(num_points);
+        float theta = 2.0 * M_PI * static_cast<float>(i) / static_cast<double>(num_points);
         
         // 円周上の点の座標を計算
-        double perfect_x = radius * std::cos(theta);
-        double perfect_y = radius * std::sin(theta);
+        float perfect_x = radius * std::cos(theta);
+        float perfect_y = radius * std::sin(theta);
         
         // ノイズを加えた最終的な座標
         std::vector<float> point = {
-            perfect_x + d(gen), // x座標 + ノイズ
-            perfect_y + d(gen)  // y座標 + ノイズ
+            perfect_x + (float)d(gen), // x座標 + ノイズ
+            perfect_y + (float)d(gen)  // y座標 + ノイズ
         };
         
         point_cloud.push_back(point);
@@ -44,5 +44,7 @@ int main()
 
     std::vector<std::vector<float>> point_cloud = generateCirclePointCloud(200, 500, 10);
     std::vector<std::vector<float>> circle_center = test_ransac.run(point_cloud);
+
+
     return 0;
 }
