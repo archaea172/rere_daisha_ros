b72d8a44f78ef73956938d9622daa42f9922b333
aky <akiyugata271@gmail.com>
2025-09-21 21:13:58 +0900

    update

--- src/rere_daisha_ros/__init__.py
+++ src/rere_daisha_ros/__init__.py
@@ -1,2 +1,4 @@
+from .yolo_test import main_yolo_test
+
 def hello() -> str:
     return "Hello from rere-daisha-ros!"
--- src/rere_daisha_ros/yolo_test.py
+++ src/rere_daisha_ros/yolo_test.py
@@ -1,6 +1,9 @@
 import cv2
+import os
+from ament_index_python.packages import get_package_share_directory
 
 from ultralytics import YOLO
 
-def main():
+def main_yolo_test():
+    package_share_directory = get_package_share_directory('rere_daisha_ros')
     model = YOLO()
