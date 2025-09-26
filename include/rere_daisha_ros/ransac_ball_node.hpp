1753a12362b9503fd3a4f1a40537116384834799
aky <akiyugata271@gmail.com>
2025-09-27 01:43:45 +0900

    update

--- include/rere_daisha_ros/ransac_ball_node.hpp
+++ include/rere_daisha_ros/ransac_ball_node.hpp
@@ -1,3 +1,14 @@
 #pragma once
 
 #include "ransac_ball.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+
+using std::placeholders::_1;
+using namespace std::chrono_literals;
+
+class RansacBallNode : public rclcpp_lifecycle::LifecycleNode
+{
+
+};
