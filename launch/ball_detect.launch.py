85bc1c781c3df7b039ca30046542bbcfe8c73542
aky <akiyugata271@gmail.com>
2025-09-26 23:32:07 +0900

    add node

--- launch/ball_detect.launch.py
+++ launch/ball_detect.launch.py
@@ -4,6 +4,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import PushRosNamespace
+from launch_ros.actions import Node
 
 def generate_launch_description():
     name_space = 'daisha'
