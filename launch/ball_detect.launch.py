c3ed934a31cff42a861f51749d2efe4270bd65bc
aky <akiyugata271@gmail.com>
2025-09-27 00:07:23 +0900

    update

--- launch/ball_detect.launch.py
+++ launch/ball_detect.launch.py
@@ -36,29 +36,41 @@ def generate_launch_description():
     ld.add_action(realsense_launch)
 
     # lidar
-    ldlidar_node = Node(
-        package='ldlidar_node',
-        executable='ldlidar_node',
-        namespace='daisha',
-        output='screen',
-        parameters=[{
-            'serial_port': '/dev/ttyUSB0',
-            'topic_name': 'scan',
-            'lidar_frame': 'ldlidar_link',
-            'range_threshold': 0.005
-        }]
+    lidar_group = GroupAction(
+        actions=[
+            # グループ内のアクションに'daisha'名前空間を適用
+            PushRosNamespace(name_space),
+
+            # Lidar Node
+            Node(
+                package='ldlidar_node',
+                executable='ldlidar_node',
+                name='ldlidar_node',  # lifecycle_managerが名前でノードを特定できるようにnameを設定
+                output='screen',
+                parameters=[{
+                    'serial_port': '/dev/ttyUSB0',
+                    'topic_name': 'scan',
+                    'lidar_frame': 'ldlidar_link',
+                    'range_threshold': 0.005
+                }]
+            ),
+            
+            # Lifecycle Manager
+            # lidar_nodeと同じグループに入れることで、同じ名前空間で動作する
+            Node(
+                package='nav2_lifecycle_manager',
+                executable='lifecycle_manager',
+                name='lifecycle_manager',
+                output='screen',
+                parameters=[
+                    {'autostart': True},
+                    # lifecycle_managerは同じnamespace内のノード名を探すので、これでOK
+                    {'node_names': ['ldlidar_node']}
+                ]
+            )
+        ]
     )
-    lifecycle_manager = Node(
-        package='nav2_lifecycle_manager',
-        executable='lifecycle_manager',
-        name='lifecycle_manager',
-        namespace='daisha',
-        output='screen',
-        parameters=[{'autostart': True},
-                    {'node_names': ['ldlidar_node']}]
-    )
-    ld.add_action(ldlidar_node)
-    ld.add_action(lifecycle_manager)
+    ld.add_action(lidar_group)
 
     # yolo
 
