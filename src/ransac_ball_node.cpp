ba287ed95bb99a2f8f905348c8a250a655401fd1
aky <akiyugata271@gmail.com>
2025-09-27 17:26:19 +0900

    update ransac initialize

--- src/ransac_ball_node.cpp
+++ src/ransac_ball_node.cpp
@@ -3,7 +3,18 @@
 RansacBallNode::RansacBallNode()
 : rclcpp_lifecycle::LifecycleNode(std::string("ransac_ball_node"))
 {
-
+    /*ransac initialize begin*/
+    float ball_r = 0.08;
+    int max_loop = 100;
+    float threshold = 0.02;
+    int min_samples = 50;
+    ransac_ball = std::make_unique<RansacBall>(
+        ball_r,
+        max_loop,
+        threshold,
+        min_samples
+    );
+    /*ransac initialize end*/
 }
 
 int main()
