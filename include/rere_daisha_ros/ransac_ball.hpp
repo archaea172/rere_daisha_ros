d16acbed8f46a2421640e492e58b27bfcc12a5cb
aky <akiyugata271@gmail.com>
2025-09-28 14:19:08 +0900

    add declare set func

--- include/rere_daisha_ros/ransac_ball.hpp
+++ include/rere_daisha_ros/ransac_ball.hpp
@@ -12,6 +12,11 @@ public:
     RansacBall(float r, int max_loop, float threshold, int min_samples);
     std::vector<std::vector<float>> run(const std::vector<std::vector<float>> &points);
 
+    void set_ball_r(float new_ball_r);
+    void set_max_loop(int new_max_loop);
+    void set_threshold(float threshold);
+    void set_min_samples(int new_min_samples);
+
 private:
     std::vector<int> sampring(uint max_val, uint num);
 
