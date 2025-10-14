8860eb7984501acf9480e34e65dfc949c8bba5e8
aky <akiyugata271@gmail.com>
2025-10-14 11:49:48 +0800

    add member value

--- src/rere_daisha_ros/ball_detect_rdk.py
+++ src/rere_daisha_ros/ball_detect_rdk.py
@@ -6,3 +6,13 @@ from scipy.special import softmax
 
 from hobot_dnn import pyeasy_dnn as dnn
 
+class Ultralytics_YOLO_Detect_Bayese_YUV420SP():
+    def __init__(self, model_path, classes_num, nms_thres, score_thres, reg, strides):
+        #init
+        self.REG = reg
+        self.CLASSES_NUM = classes_num
+        self.SCORE_THRESHOLD = score_thres
+        self.NMS_THRESHOLD = nms_thres
+        self.CONF_THRES_RAW = -np.log(1/self.SCORE_THRESHOLD - 1)
+        self.input_H, self.input_W = self.quantize_model[0].inputs[0].properties.shape[2:4]
+        self.strides = strides
