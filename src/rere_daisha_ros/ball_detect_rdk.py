e6ab35aba7f24ab5533d9592772829c8b9a1329a
aky <akiyugata271@gmail.com>
2025-10-14 12:15:45 +0800

    add function

--- src/rere_daisha_ros/ball_detect_rdk.py
+++ src/rere_daisha_ros/ball_detect_rdk.py
@@ -27,3 +27,62 @@ class Ultralytics_YOLO_Detect_Bayese_YUV420SP():
             grid_H, grid_W = self.input_H // stride, self.input_W // stride
             self.grids.append(np.stack([np.tile(np.linspace(0.5, grid_H-0.5, grid_H), reps=grid_H), 
                             np.repeat(np.arange(0.5, grid_W+0.5, 1), grid_W)], axis=0).transpose(1,0))
+
+    def preprocess_yuv420sp(self, img):
+        RESIZE_TYPE = 0
+        LETTERBOX_TYPE = 1
+        PREPROCESS_TYPE = LETTERBOX_TYPE
+
+        self.img_h, self.img_w = img.shape[0:2]
+        if PREPROCESS_TYPE == RESIZE_TYPE:
+            # 利用resize的方式进行前处理, 准备nv12的输入数据
+            input_tensor = cv2.resize(img, (self.input_W, self.input_H), interpolation=cv2.INTER_NEAREST) # 利用resize重新开辟内存节约一次
+            input_tensor = self.bgr2nv12(input_tensor)
+            self.y_scale = 1.0 * self.input_H / self.img_h
+            self.x_scale = 1.0 * self.input_W / self.img_w
+            self.y_shift = 0
+            self.x_shift = 0
+        elif PREPROCESS_TYPE == LETTERBOX_TYPE:
+            # 利用 letter box 的方式进行前处理, 准备nv12的输入数据
+            self.x_scale = min(1.0 * self.input_H / self.img_h, 1.0 * self.input_W / self.img_w)
+            self.y_scale = self.x_scale
+            
+            if self.x_scale <= 0 or self.y_scale <= 0:
+                raise ValueError("Invalid scale factor.")
+            
+            new_w = int(self.img_w * self.x_scale)
+            self.x_shift = (self.input_W - new_w) // 2
+            x_other = self.input_W - new_w - self.x_shift
+            
+            new_h = int(self.img_h * self.y_scale)
+            self.y_shift = (self.input_H - new_h) // 2
+            y_other = self.input_H - new_h - self.y_shift
+            
+            input_tensor = cv2.resize(img, (new_w, new_h))
+            input_tensor = cv2.copyMakeBorder(input_tensor, self.y_shift, y_other, self.x_shift, x_other, cv2.BORDER_CONSTANT, value=[127, 127, 127])
+            input_tensor = self.bgr2nv12(input_tensor)
+        else:
+            exit(-1)
+
+        return input_tensor
+    
+    def bgr2nv12(self, bgr_img):
+        height, width = bgr_img.shape[0], bgr_img.shape[1]
+        area = height * width
+        yuv420p = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2YUV_I420).reshape((area * 3 // 2,))
+        y = yuv420p[:area]
+        uv_planar = yuv420p[area:].reshape((2, area // 4))
+        uv_packed = uv_planar.transpose((1, 0)).reshape((area // 2,))
+        nv12 = np.zeros_like(yuv420p)
+        nv12[:height * width] = y
+        nv12[height * width:] = uv_packed
+        return nv12
+    
+    def forward(self, input_tensor):
+        quantize_outputs = self.quantize_model[0].forward(input_tensor)
+        return quantize_outputs
+
+    def c2numpy(self, outputs):
+        outputs = [dnnTensor.buffer for dnnTensor in outputs]
+        return outputs
+    
