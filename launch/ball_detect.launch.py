240a5f87b63aa399fbb79fcbede5c73d8c32a856
aky <akiyugata271@gmail.com>
2025-10-21 09:28:36 +0900

    comment out realsense

--- launch/ball_detect.launch.py
+++ launch/ball_detect.launch.py
@@ -14,28 +14,28 @@ def generate_launch_description():
     ld = LaunchDescription()
 
     # realsense
-    realsense_launch_file = os.path.join(
-        get_package_share_directory('realsense2_camera'),
-        'launch',
-        'rs_launch.py'
-    )
-    realsense_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(realsense_launch_file),
-        launch_arguments={
-            'camera_namespace': name_space,
-            'camera_name': 'D435',
-            'enable_rgbd': 'true',
-            'enable_sync': 'true',
-            'align_depth.enable': 'true',
-            'enable_color': 'true',
-            'enable_depth': 'true',
-            'enable_gyro': 'true',
-            'enable_accel': 'true',
-            'device_type': 'd435',
-            'pointcloud.enable': 'true'   
-        }.items()
-    )
-    ld.add_action(realsense_launch)
+    # realsense_launch_file = os.path.join(
+    #     get_package_share_directory('realsense2_camera'),
+    #     'launch',
+    #     'rs_launch.py'
+    # )
+    # realsense_launch = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(realsense_launch_file),
+    #     launch_arguments={
+    #         'camera_namespace': name_space,
+    #         'camera_name': 'D435',
+    #         'enable_rgbd': 'true',
+    #         'enable_sync': 'true',
+    #         'align_depth.enable': 'true',
+    #         'enable_color': 'true',
+    #         'enable_depth': 'true',
+    #         'enable_gyro': 'true',
+    #         'enable_accel': 'true',
+    #         'device_type': 'd435',
+    #         'pointcloud.enable': 'true'   
+    #     }.items()
+    # )
+    # ld.add_action(realsense_launch)
 
     # lidar
 
