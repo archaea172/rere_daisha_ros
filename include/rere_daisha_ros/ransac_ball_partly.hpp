95857ebbcf72f463ec2f96ca3fad6802c094201c
aky <akiyugata271@gmail.com>
2025-10-22 17:27:59 +0900

    add member func

--- include/rere_daisha_ros/ransac_ball_partly.hpp
+++ include/rere_daisha_ros/ransac_ball_partly.hpp
@@ -15,5 +15,23 @@ using namespace std::chrono_literals;
 
 class RansacBallPartlyNode : public rclcpp_lifecycle::LifecycleNode
 {
+public:
+    /*type define begin*/
+    using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+    /*type define end*/
+
+    /*class func begin*/
+    RansacBallPartlyNode();
+    /*class func end*/
+    
+private:
+    /*lifecycle callback begin*/
+    CallbackReturn on_configure(const rclcpp_lifecycle::State &state);
+    CallbackReturn on_activate(const rclcpp_lifecycle::State &state);
+    CallbackReturn on_deactivate(const rclcpp_lifecycle::State &state);
+    CallbackReturn on_cleanup(const rclcpp_lifecycle::State &state);
+    CallbackReturn on_error(const rclcpp_lifecycle::State &state);
+    CallbackReturn on_shutdown(const rclcpp_lifecycle::State &state);
+    /*lifecycle callback end*/
 
 };
