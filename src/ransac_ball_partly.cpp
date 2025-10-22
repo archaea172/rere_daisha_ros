9a74e20709a358d721b92f3f3b67c4d190822d1e
aky <akiyugata271@gmail.com>
2025-10-22 17:32:24 +0900

    add main func

--- src/ransac_ball_partly.cpp
+++ src/ransac_ball_partly.cpp
@@ -1,6 +1,10 @@
 #include "ransac_ball_partly.hpp"
 
-int main()
+int main(int argc, char *argv[])
 {
+    rclcpp::init(argc, argv);
+    std::shared_ptr<RansacBallNode> node = std::make_shared<RansacBallPartlyNode>();
+    rclcpp::spin(node->get_node_base_interface());
+    rclcpp::shutdown();
     return 0;
 }
