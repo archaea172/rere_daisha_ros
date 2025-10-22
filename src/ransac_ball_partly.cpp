35ac2a019b3325259115ac1617e4e7d31cac7c2e
aky <akiyugata271@gmail.com>
2025-10-22 17:20:34 +0900

    update

--- src/ransac_ball_partly.cpp
+++ src/ransac_ball_partly.cpp
@@ -1,14 +1,6 @@
-#pragma once
+#include "ransac_ball_partly.hpp"
 
-#include "ransac_ball.hpp"
-
-#include "cmath"
-
-#include "rclcpp/rclcpp.hpp"
-#include "rclcpp_lifecycle/lifecycle_node.hpp"
-
-#include "sensor_msgs/msg/laser_scan.hpp"
-#include "rere_daisha_msgs/msg/ball_position_array.hpp"
-
-using std::placeholders::_1;
-using namespace std::chrono_literals;
+int main()
+{
+    return 0;
+}
