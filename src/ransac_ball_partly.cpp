e9173f2441bd887b538a3fbddedba37c3d5eefc5
aky <akiyugata271@gmail.com>
2025-10-24 23:49:43 +0900

    add publishing

--- src/ransac_ball_partly.cpp
+++ src/ransac_ball_partly.cpp
@@ -159,7 +159,7 @@ void RansacBallPartlyNode::ransac_timer_callback()
     rere_daisha_msgs::msg::BallPosition txdata;
     txdata.class_id = this->ball_position_array_.balls[nearest_index].class_id;
 
-    if (this->nearest_ball_position->is_activated()) this->nearest_ball_position.publish(txdata);
+    if (this->nearest_ball_position->is_activated()) this->nearest_ball_position->publish(txdata);
 }
 
 rcl_interfaces::msg::SetParametersResult RansacBallPartlyNode::parameters_callback(
