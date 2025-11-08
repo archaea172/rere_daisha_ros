15fe180c69c56777467d308d524c3dff4ba2e48f
aky <akiyugata271@gmail.com>
2025-11-09 00:31:11 +0900

    feat: add namespace and package path initialization in launch description

--- launch/ball_detector.launch.py
+++ launch/ball_detector.launch.py
@@ -1,5 +1,9 @@
 from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    name_space = 'daisha'
+    pkg_path = get_package_share_directory('rere_daisha_ros')
+
     ld = LaunchDescription()
     return ld
