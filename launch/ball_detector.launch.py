0fb4964bcf6232b95610a9ca523d5092ed62131b
aky <akiyugata271@gmail.com>
2025-11-09 01:38:51 +0900

    feat: update ball_detector.launch.py to modify camera transform parameters

--- launch/ball_detector.launch.py
+++ launch/ball_detector.launch.py
@@ -2,6 +2,8 @@ from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 
+import numpy as np
+
 def generate_launch_description():
     name_space = 'rere_daisha'
     pkg_path = get_package_share_directory('rere_daisha_ros')
@@ -17,13 +19,15 @@ def generate_launch_description():
     ld.add_action(yolo_rdk_node)
 
     # カメラ用のtf
+    rad = 120
+    theta = np.deg2rad(rad)
     tf2_camera_node = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
         name='base_to_camera_static_publisher',
             arguments=[
-                '0.2', '0.0', '0.1',
-                '0.0', '-0.785398', '0.0',
+                '0.0', '-0.03', '0.1938',
+                str(theta), '0.0', '0.0',
                 'base_link', 
                 'camera_link'
             ]
