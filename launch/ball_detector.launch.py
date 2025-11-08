79ff8e89e929a3f95fc142a47e1271c0c155a3ea
aky <akiyugata271@gmail.com>
2025-11-09 00:35:32 +0900

    feat: add Node initialization for yolo_rdk_node in launch description

--- launch/ball_detector.launch.py
+++ launch/ball_detector.launch.py
@@ -1,9 +1,13 @@
 from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
 
 def generate_launch_description():
     name_space = 'rere_daisha'
     pkg_path = get_package_share_directory('rere_daisha_ros')
 
     ld = LaunchDescription()
+
+    # yolo
+    yolo_rdk_node = Node()
     return ld
