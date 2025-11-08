7d26d5262a52768c468173292ecd646078e73c8d
aky <akiyugata271@gmail.com>
2025-11-09 00:34:14 +0900

    change robot name

--- launch/ball_detector.launch.py
+++ launch/ball_detector.launch.py
@@ -2,7 +2,7 @@ from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    name_space = 'daisha'
+    name_space = 'rere_daisha'
     pkg_path = get_package_share_directory('rere_daisha_ros')
 
     ld = LaunchDescription()
