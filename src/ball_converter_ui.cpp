95eed3861210ad9c9f9267c5d531f29bdc590514
aky <akiyugata271@gmail.com>
2025-11-09 17:37:13 +0900

    feat: initialize ball_yolo_subscriber and ball_yolo_ui_publisher in BallConverterListenerNode constructor

--- src/ball_converter_ui.cpp
+++ src/ball_converter_ui.cpp
@@ -3,12 +3,20 @@
 BallConverterListenerNode::BallConverterListenerNode()
 : rclcpp::Node(std::string("ball_converter_listener_node"))
 {
-
+    this->ball_yolo_subscriber = this->create_subscription<rere_daisha_msgs::msg::BallPositionArray>(
+        std::string("ball_position_yolo"),
+        rclcpp::SystemDefaultsQoS(),
+        std::bind(&BallConverterListenerNode::yolo_ball_callback, this, _1)
+    );
+    this->ball_yolo_ui_publisher = this->create_publisher(
+        std::string("ball_position_yolo_ui"),
+        rclcpp::SystemDefaultsQoS()
+    );
 }
 
 void BallConverterListenerNode::yolo_ball_callback(const rere_daisha_msgs::msg::BallPositionArray::SharedPtr rxdata)
 {
-    
+
 }
 
 int main(int argc, char * argv[])
