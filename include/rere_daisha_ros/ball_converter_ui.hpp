4d30d7943c2843c9675dcbbf0390933a920f4388
aky <akiyugata271@gmail.com>
2025-11-09 18:19:34 +0900

    feat: add tf2/convert.h include to ball_converter_ui.hpp

--- include/rere_daisha_ros/ball_converter_ui.hpp
+++ include/rere_daisha_ros/ball_converter_ui.hpp
@@ -6,6 +6,7 @@
 #include "tf2_ros/buffer.h"
 #include "tf2_ros/transform_listener.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "tf2/convert.h"
 
 #include "rere_daisha_msgs/msg/ball_position_array.hpp"
 
