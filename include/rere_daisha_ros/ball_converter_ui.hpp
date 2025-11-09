172cb5c593d8d0a22155271de075d109ce1d2826
aky <akiyugata271@gmail.com>
2025-11-09 16:35:50 +0900

    feat: add yolo_ball_callback method and include ball_position_array message

--- include/rere_daisha_ros/ball_converter_ui.hpp
+++ include/rere_daisha_ros/ball_converter_ui.hpp
@@ -7,6 +7,8 @@
 #include "tf2_ros/transform_listener.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 
+#include "rere_daisha_msgs/msg/ball_position_array.hpp"
+
 using std::placeholders::_1;
 using namespace std::chrono_literals;
 
@@ -15,6 +17,8 @@ class BallConverterListenerNode : public rclcpp::Node
 private:
     BallConverterListenerNode();
 public:
+    void yolo_ball_callback(const rere_daisha_msgs::msg::BallPositionArray::SharedPtr rxdata);
+
     std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
     std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
 };
