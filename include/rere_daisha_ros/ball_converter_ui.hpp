dd0bfdbbd50f411c0a6fa7efe0f64a9c721376eb
aky <akiyugata271@gmail.com>
2025-11-09 16:13:59 +0900

    feat: implement BallConverterListenerNode class in ball_converter_ui.hpp

--- include/rere_daisha_ros/ball_converter_ui.hpp
+++ include/rere_daisha_ros/ball_converter_ui.hpp
@@ -7,3 +7,8 @@
 #include "tf2_ros/transform_listener.h"
 #include "geometry_msgs/msg/point_stamped.hpp"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+
+class BallConverterListenerNode : public rclcpp::Node
+{
+
+};
