45c5e5b4d4939ed60abea5591d714a7731ae06fe
aky <akiyugata271@gmail.com>
2025-11-09 16:31:51 +0900

    feat: remove unused include for geometry_msgs in ball_converter_ui.hpp

--- include/rere_daisha_ros/ball_converter_ui.hpp
+++ include/rere_daisha_ros/ball_converter_ui.hpp
@@ -5,8 +5,10 @@
 
 #include "tf2_ros/buffer.h"
 #include "tf2_ros/transform_listener.h"
-#include "geometry_msgs/msg/point_stamped.hpp"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+
+using std::placeholders::_1;
+using namespace std::chrono_literals;
 
 class BallConverterListenerNode : public rclcpp::Node
 {
