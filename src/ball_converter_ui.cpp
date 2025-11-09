6bba83f21e96a8b7290033606a91e4852d4e48e6
aky <akiyugata271@gmail.com>
2025-11-09 17:24:42 +0900

    feat: implement constructor for BallConverterListenerNode

--- src/ball_converter_ui.cpp
+++ src/ball_converter_ui.cpp
@@ -1,5 +1,11 @@
 #include "ball_converter_ui.hpp"
 
+BallConverterListenerNode::BallConverterListenerNode()
+: rclcpp::Node(std::string("ball_converter_listener_node"))
+{
+
+}
+
 int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
