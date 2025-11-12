50c748e47380e7cd762b0e9d50a7c1333b2d0e3f
aky <akiyugata271@gmail.com>
2025-11-12 17:32:14 +0900

    update

--- src/mppi_pursuit.cpp
+++ src/mppi_pursuit.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-    MPPIControler debug(100, 2);
+    MPPIControler debug(3, 2);
     auto matrix = debug.sampling_dim2();
 
     std::cout << matrix << std::endl;
