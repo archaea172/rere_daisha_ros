a377ff36fe27c8b8337bb115547308708f5b6219
aky <akiyugata271@gmail.com>
2025-11-12 11:33:24 +0900

    add sampring value

--- include/rere_daisha_ros/mppi.hpp
+++ include/rere_daisha_ros/mppi.hpp
@@ -9,6 +9,13 @@ class MPPIControler
     public:
         MPPIControler(int sample_num, int dim_num);
     private:
+
         int sample_num_;
         int dim_num_;
+
+        Eigen::VectorXd mu;
+        Eigen::MatrixXd sig;
+
+        std::mt19937_64 gen;
+        std::uniform_real_distribution<double> uni;
 };
