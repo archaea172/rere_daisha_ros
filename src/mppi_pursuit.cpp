55e198f157da74c5ab4445b1dcc73ff79e655aae
aky <akiyugata271@gmail.com>
2025-11-12 19:29:46 +0900

    fix

--- src/mppi_pursuit.cpp
+++ src/mppi_pursuit.cpp
@@ -5,8 +5,7 @@ int main(int argc, char *argv[])
 {
     MPPIControler debug(3, 2);
     auto matrix = debug.sampling_dim2();
+    std::cout << matrix << std::endl;
     Eigen::VectorXd init;
     debug.predict(init, matrix);
-
-    std::cout << matrix << std::endl;
 }
