9e26e314ce21806792d47e6e913434e23ce88558
aky <akiyugata271@gmail.com>
2025-11-13 22:44:03 +0900

    update

--- src/mppi.cpp
+++ src/mppi.cpp
@@ -15,12 +15,14 @@ d(0.5), dt(0.1), loop_num_(1000)
 Eigen::VectorXd MPPIControler::run()
 {
     Eigen::VectorXd evaluation_result(this->loop_num_);
+    Eigen::MatrixXd input_first(3, this->loop_num_);
 
     for (size_t i = 0; i < this->loop_num_; i++)
     {
         Eigen::MatrixXd input_sample = this->sampling_dim2();
         Eigen::MatrixXd state_sample = this->predict(this->state, input_sample);
-        double c = evaluation(state_sample, input_sample);
+        evaluation_result[i] = evaluation(state_sample, input_sample);
+        input_first.col(i) = input_sample.col(i);
     }
 
 }
