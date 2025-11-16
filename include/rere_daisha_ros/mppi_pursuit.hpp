3f50d16229578ed4fa6b0a17dab772ea68dbcd01
aky <akiyugata271@gmail.com>
2025-11-17 03:39:22 +0900

    add header file

---
+++ include/rere_daisha_ros/mppi_pursuit.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+
+using std::placeholders::_1;
+using namespace std::chrono_literals;
+
+class MPPIPursuitNode : rclcpp_lifecycle::LifecycleNode
+{
+
+};
