70f7dc1d5f77e1d16d1ac388dbd3c07bc84041c1
aky <akiyugata271@gmail.com>
2025-11-17 03:58:18 +0900

    add indent

--- include/rere_daisha_ros/mppi_pursuit.hpp
+++ include/rere_daisha_ros/mppi_pursuit.hpp
@@ -14,12 +14,12 @@ class MPPIPursuitNode : rclcpp_lifecycle::LifecycleNode
         using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
         MPPIPursuitNode();
     private:
-    CallbackReturn on_configure(const rclcpp_lifecycle::State &state);
-    CallbackReturn on_activate(const rclcpp_lifecycle::State &state);
-    CallbackReturn on_deactivate(const rclcpp_lifecycle::State &state);
-    CallbackReturn on_cleanup(const rclcpp_lifecycle::State &state);
-    CallbackReturn on_error(const rclcpp_lifecycle::State &state);
-    CallbackReturn on_shutdown(const rclcpp_lifecycle::State &state);
+        CallbackReturn on_configure(const rclcpp_lifecycle::State &state);
+        CallbackReturn on_activate(const rclcpp_lifecycle::State &state);
+        CallbackReturn on_deactivate(const rclcpp_lifecycle::State &state);
+        CallbackReturn on_cleanup(const rclcpp_lifecycle::State &state);
+        CallbackReturn on_error(const rclcpp_lifecycle::State &state);
+        CallbackReturn on_shutdown(const rclcpp_lifecycle::State &state);
 
-    std::unique_ptr<MPPIControler> mppi_controler;
+        std::unique_ptr<MPPIControler> mppi_controler;
 };
