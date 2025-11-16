45d2d4c69f3fa0abb965949f58e59354b2e6b07c
aky <akiyugata271@gmail.com>
2025-11-17 04:08:54 +0900

    update func

--- src/mppi_pursuit.cpp
+++ src/mppi_pursuit.cpp
@@ -6,6 +6,37 @@ MPPIPursuitNode::MPPIPursuitNode()
 
 }
 
+MPPIPursuitNode::CallbackReturn MPPIPursuitNode::on_configure(const rclcpp_lifecycle::State &state)
+{
+
+}
+
+MPPIPursuitNode::CallbackReturn MPPIPursuitNode::on_activate(const rclcpp_lifecycle::State &state)
+{
+
+}
+
+MPPIPursuitNode::CallbackReturn MPPIPursuitNode::on_deactivate(const rclcpp_lifecycle::State &state)
+{
+    
+}
+
+MPPIPursuitNode::CallbackReturn MPPIPursuitNode::on_cleanup(const rclcpp_lifecycle::State &state)
+{
+    
+}
+
+MPPIPursuitNode::CallbackReturn MPPIPursuitNode::on_error(const rclcpp_lifecycle::State &state)
+{
+    
+}
+
+MPPIPursuitNode::CallbackReturn MPPIPursuitNode::on_shutdown(const rclcpp_lifecycle::State &state)
+{
+    
+}
+
+
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
