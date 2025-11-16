e6f73ca582d99ac9672dc24183d5156797679dcf
aky <akiyugata271@gmail.com>
2025-11-17 04:02:07 +0900

    add value

--- include/rere_daisha_ros/mppi_pursuit.hpp
+++ include/rere_daisha_ros/mppi_pursuit.hpp
@@ -2,6 +2,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include <Eigen/Dense>
 
 #include "mppi.hpp"
 
@@ -22,4 +23,19 @@ class MPPIPursuitNode : rclcpp_lifecycle::LifecycleNode
         CallbackReturn on_shutdown(const rclcpp_lifecycle::State &state);
 
         std::unique_ptr<MPPIControler> mppi_controler;
+
+        OnSetParametersCallbackHandle::SharedPtr parameter_callback_hanle_;
+        rcl_interfaces::msg::SetParametersResult parameters_callback(
+            const std::vector<rclcpp::Parameter> &parameters
+        );
+        
+        int sample_num;
+        int dim_num;
+        int loop_num;
+        double dt;
+        Eigen::MatrixXd sig_;
+        double max_wheel_vel;
+        double wheel_distance;
+        Eigen::VectorXd weight_array;
+        double lambda_;
 };
