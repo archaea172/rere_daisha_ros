ce6068ada136479d3afe2b4de0c731a46149291c
aky <akiyugata271@gmail.com>
2025-11-17 02:06:47 +0900

    update

--- src/mppi_pursuit.cpp
+++ src/mppi_pursuit.cpp
@@ -1,5 +1,6 @@
 #include "mppi.hpp"
 #include <iostream>
+#include <chrono>
 
 int main(int argc, char *argv[])
 {
@@ -12,9 +13,12 @@ int main(int argc, char *argv[])
 
     Eigen::VectorXd state(3);
     state << 0, 0, 0;
-    Eigen::VectorXd pos_ref(3);
-    pos_ref << 1, 0;
-
+    Eigen::VectorXd pos_ref(2);
+    pos_ref << 0, 1;
+    auto start = std::chrono::high_resolution_clock::now();
     Eigen::VectorXd input = debug.run(state, pos_ref);
+    auto end = std::chrono::high_resolution_clock::now();
     std::cout << input << std::endl;
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << duration.count() << std::endl;
 }
