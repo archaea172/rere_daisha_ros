744afe4118895adac6274991c31e059694b33f5f
aky <akiyugata271@gmail.com>
2025-11-19 16:28:24 +0900

    add class

---
+++ include/rere_daisha_ros/yasarobo_bridge.hpp
@@ -0,0 +1,13 @@
+#include "ros2can_bridge.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+
+class Ros2CanBridge
+: public rclcpp_lifecycle::LifecycleNode
+{
+public:
+    Ros2CanBridge();
+private:
+    
+};
