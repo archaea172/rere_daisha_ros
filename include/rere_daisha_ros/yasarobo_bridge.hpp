8a560e97fa95af774dc4948f11c62202eb9359ab
aky <akiyugata271@gmail.com>
2025-11-19 16:35:28 +0900

    add private

--- include/rere_daisha_ros/yasarobo_bridge.hpp
+++ include/rere_daisha_ros/yasarobo_bridge.hpp
@@ -3,11 +3,26 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
 
+#include "std_msgs/msg/float64_multi_array.hpp"
+
 class Ros2CanBridge
 : public rclcpp_lifecycle::LifecycleNode
 {
 public:
     Ros2CanBridge();
+    using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
 private:
-    
+    CallbackReturn on_configure(const rclcpp_lifecycle::State &state);
+    CallbackReturn on_activate(const rclcpp_lifecycle::State &state);
+    CallbackReturn on_deactivate(const rclcpp_lifecycle::State &state);
+    CallbackReturn on_cleanup(const rclcpp_lifecycle::State &state);
+    CallbackReturn on_error(const rclcpp_lifecycle::State &state);
+    CallbackReturn on_shutdown(const rclcpp_lifecycle::State &state);
+
+    std::unique_ptr<CanBridge> bridge;
+
+    void vel_callback(const std_msgs::msg::Float64MultiArray::SharedPtr rxdata);
+
+    rclcpp::Subscription<std_msgs::msg::Float64MultiArray>::SharedPtr input_subscriber;
+    const char* Ifname;
 };
