d753c81558af0755fdc3624bf754d228626bbc75
aky <akiyugata271@gmail.com>
2025-11-20 14:28:18 +0900

    update

--- include/rere_daisha_ros/convert_diff_cmd_vel.hpp
+++ include/rere_daisha_ros/convert_diff_cmd_vel.hpp
@@ -5,14 +5,17 @@
 #include "std_msgs/msg/float64_multi_array.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 
+using std::placeholders::_1;
+using namespace std::chrono_literals;
+
 class ConvertDiffCmdVel
-: rclcpp::Node
+: public rclcpp::Node
 {
 public:
     ConvertDiffCmdVel();
 private:
     void wheel_vel_callback(const std_msgs::msg::Float64MultiArray::SharedPtr rxdata);
-    
+
     rclcpp::Subscription<std_msgs::msg::Float64MultiArray>::SharedPtr wheel_vel_subscriber;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_publisher;
 };
--- src/convert_diff_cmd_vel.cpp
+++ src/convert_diff_cmd_vel.cpp
@@ -3,5 +3,22 @@
 ConvertDiffCmdVel::ConvertDiffCmdVel()
 : rclcpp::Node(std::string("convert_diff_cmd_vel"))
 {
+    this->cmd_vel_publisher = this->create_publisher<geometry_msgs::msg::Twist>(
+        std::string("cmd_vel_feedback"),
+        rclcpp::SystemDefaultsQoS()
+    );
+    this->wheel_vel_subscriber = this->create_subscription<std_msgs::msg::Float64MultiArray>(
+        std::string("wheel_vel"),
+        rclcpp::SystemDefaultsQoS(),
+        std::bind(&ConvertDiffCmdVel::wheel_vel_callback, this, _1)
+    );
+}
 
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    std::shared_ptr<ConvertDiffCmdVel> node = std::make_shared<ConvertDiffCmdVel>();
+    rclcpp::spin(node->get_node_base_interface());
+    rclcpp::shutdown();
+    return 0;
 }
