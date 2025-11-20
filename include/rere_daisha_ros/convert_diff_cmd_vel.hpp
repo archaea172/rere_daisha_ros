e2a8050d6360752e83a529cdaaa86cd35e6ba62b
aky <akiyugata271@gmail.com>
2025-11-20 14:17:47 +0900

    update class

--- include/rere_daisha_ros/convert_diff_cmd_vel.hpp
+++ include/rere_daisha_ros/convert_diff_cmd_vel.hpp
@@ -2,11 +2,17 @@
 
 #include "rclcpp/rclcpp.hpp"
 
-#include "std_msgs/msg/float32_multi_array.hpp"
+#include "std_msgs/msg/float64_multi_array.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 
 class ConvertDiffCmdVel
 : rclcpp::Node
 {
-
+public:
+    ConvertDiffCmdVel();
+private:
+    void wheel_vel_callback(const std_msgs::msg::Float64MultiArray::SharedPtr rxdata);
+    
+    rclcpp::Subscription<std_msgs::msg::Float64MultiArray>::SharedPtr wheel_vel_subscriber;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_publisher;
 };
