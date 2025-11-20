a635a89142c05687d12e2888dbb22b89c78e589d
aky <akiyugata271@gmail.com>
2025-11-20 14:40:34 +0900

    update

--- src/convert_diff_cmd_vel.cpp
+++ src/convert_diff_cmd_vel.cpp
@@ -3,10 +3,14 @@
 ConvertDiffCmdVel::ConvertDiffCmdVel()
 : rclcpp::Node(std::string("convert_diff_cmd_vel"))
 {
+    this->declare_parameter<double>("wheel_distance", 0.5);
+    this->wheel_distance = this->get_parameter("wheel_distance").as_double();
+
     this->cmd_vel_publisher = this->create_publisher<geometry_msgs::msg::Twist>(
         std::string("cmd_vel_feedback"),
         rclcpp::SystemDefaultsQoS()
     );
+    
     this->wheel_vel_subscriber = this->create_subscription<std_msgs::msg::Float64MultiArray>(
         std::string("wheel_vel"),
         rclcpp::SystemDefaultsQoS(),
