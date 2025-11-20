e38dfd55f8fdd4d669c1391a1e5bc5d6683a0377
aky <akiyugata271@gmail.com>
2025-11-20 14:10:06 +0900

    add include and change class

--- include/rere_daisha_ros/convert_diff_cmd_vel.hpp
+++ include/rere_daisha_ros/convert_diff_cmd_vel.hpp
@@ -1,1 +1,12 @@
 #pragma once
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "std_msgs/msg/float32_multi_array.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+
+class ConvertDiffCmdVel
+: rclcpp::Node
+{
+
+};
