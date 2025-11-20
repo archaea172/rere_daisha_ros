a8db1bc56d4fdca04615097e465959b54e6f0598
aky <akiyugata271@gmail.com>
2025-11-20 11:11:38 +0900

    remove sampling

--- include/rere_daisha_ros/ransac_field.hpp
+++ include/rere_daisha_ros/ransac_field.hpp
@@ -9,7 +9,7 @@ public:
     RansacField();
     void run();
 private:
-    std::vector<int> sampring(uint max_val, uint num);
+    
 
     Eigen::Vector3d pose_;
     Eigen::Vector3d pose_init_;
--- src/ransac_field.cpp
+++ src/ransac_field.cpp
@@ -10,17 +10,3 @@ void RansacField::run()
 {
 
 }
-
-std::vector<int> RansacField::sampring(uint max_val, uint num)
-{
-    if (num > max_val) {
-        return {};
-    }
-    std::vector<int> numbers(max_val);
-    std::iota(numbers.begin(), numbers.end(), 0);
-    std::random_device rd;
-    std::mt19937 g(rd());
-    std::shuffle(numbers.begin(), numbers.end(), g);
-    std::vector<int> return_numbers(numbers.begin(), numbers.begin() + num);
-    return return_numbers;
-}
