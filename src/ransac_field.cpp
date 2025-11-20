08cd11d623c9bab513aaa470d616b8352f46225f
aky <akiyugata271@gmail.com>
2025-11-20 11:07:57 +0900

    update sampling

--- src/ransac_field.cpp
+++ src/ransac_field.cpp
@@ -10,3 +10,17 @@ void RansacField::run()
 {
 
 }
+
+std::vector<int> RansacBall::sampring(uint max_val, uint num)
+{
+    if (num > max_val) {
+        return {};
+    }
+    std::vector<int> numbers(max_val);
+    std::iota(numbers.begin(), numbers.end(), 0);
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(numbers.begin(), numbers.end(), g);
+    std::vector<int> return_numbers(numbers.begin(), numbers.begin() + num);
+    return return_numbers;
+}
