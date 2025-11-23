c3319ecf5e9ce936a79cdfc3101e27a6a7f5f92d
aky <akiyugata271@gmail.com>
2025-11-23 17:53:42 +0900

    add generate launch

--- launch/move.launch.py
+++ launch/move.launch.py
@@ -2,3 +2,8 @@ from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 
+def generate_launch_description():
+    name_space = 'rere_daisha'
+    ld = LaunchDescription()
+
+    
