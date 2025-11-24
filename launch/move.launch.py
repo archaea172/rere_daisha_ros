02d6ba8bd7fbef312c23463ebd8bd93891689032
aky <akiyugata271@gmail.com>
2025-11-24 16:10:21 +0900

    add mcl

--- launch/move.launch.py
+++ launch/move.launch.py
@@ -2,8 +2,34 @@ from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 
+import math
+
 def generate_launch_description():
+    x = 0.5
+    y = 0.5
+    theta = 0
+
     name_space = 'rere_daisha'
     ld = LaunchDescription()
+
+    mcl_node = Node(
+        package="yasarobo2025_26",
+        executable="mcl_node",
+        namespace=name_space,
+        parameters=[{
+            "particleNum": 50,
+            "initial_x": x,
+            "initial_y": y,
+            "initial_theta": theta,
+            "odomNoise1": 2.0,
+            "odomNoise2": 0.5,
+            "odomNoise3": 2.0,
+            "odomNoise4": 5.0,
+            "resampleThreshold": 0.9,
+            "scanStep": 5,
+            "lidar_threshold": 3.0/40.0*math.pi,
+        }],
+    )
+    ld.add_action(mcl_node)
 
     
