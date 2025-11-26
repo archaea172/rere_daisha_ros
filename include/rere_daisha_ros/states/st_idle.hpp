6bcbb9bed7278b7a8cb96011885cd56fb99f786f
aky <akiyugata271@gmail.com>
2025-11-26 21:55:34 +0900

    update

--- include/rere_daisha_ros/states/st_idle.hpp
+++ include/rere_daisha_ros/states/st_idle.hpp
@@ -7,6 +7,7 @@ using namespace cl_ros_timer;
 namespace sm_rere_daisha {
 namespace states {
 
+struct EvKeyA : sc::event<EvKeyA> {};
 // 次のStateの先行宣言
 struct StActive;
 
@@ -15,9 +16,32 @@ struct StIdle : smacc2::SmaccState<StIdle, sm_rere_daisha::SmRereDaisha> {
 
   // --- 遷移マップ (Transition Map) ---
   typedef mpl::list<
-    // EvTimerイベントが発生したら、StActiveへ遷移(Transition)する
-    Transition<EvTopicMessage<ClKeyboard, OrKeyboard>, StActive>
+    Transition<EvKeyA, StActive>
   > reactions;
+  void runtimeConfigure() {
+    ClKeyboard* client;
+    this->requiresClient(client);if (client != nullptr) {
+      client->onMessageReceived(&StIdle::onKeyReceived, this);
+    } else {
+      RCLCPP_ERROR(getLogger(), "ClKeyboard not found!");
+    }
+  }
+
+  void onKeyReceived(const std_msgs::msg::UInt16& msg) {
+    // 'a' の文字コードは 97
+    if (msg.data == 97) {
+      RCLCPP_INFO(getLogger(), "Key 'a' pressed! Posting EvKeyA.");
+      this->postEvent<EvKeyA>(); // 自作イベントを発火！ -> 遷移マップが反応する
+    }
+    // 's' の文字コードは 115
+    else if (msg.data == 115) {
+      RCLCPP_INFO(getLogger(), "Key 's' pressed. Stay in Idle.");
+      // StIdleにいる時に's'(停止)を押されても何もしない（あるいはEvKeySを投げても遷移定義がなければ無視される）
+    }
+    else {
+      RCLCPP_INFO(getLogger(), "Ignored key: %d", msg.data);
+    }
+  }
 
   // --- 状態に入った時の処理 ---
   void onEntry() {
