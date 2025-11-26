9cfea7f86fb0aca160ff22d2b7a899ed838ef584
aky <akiyugata271@gmail.com>
2025-11-27 03:01:27 +0900

    update include

--- include/rere_daisha_ros/arm_control.hpp
+++ include/rere_daisha_ros/arm_control.hpp
@@ -2,8 +2,15 @@
 
 #include "rclcpp/rclcpp.hpp"
 
+#include "geometry_msgs/msg/point_stamped.hpp"
+
 class ArmControl
 : public rclcpp::Node
 {
-
+public:
+    ArmControl();
+private:
+    double theta;
+    double r;
+    double height;
 };
