9b8bbcad5180b7d800bb2c93211d7b18ad66ce48
aky <akiyugata271@gmail.com>
2025-11-27 11:37:43 +0900

    update

--- include/rere_daisha_ros/arm_control.hpp
+++ include/rere_daisha_ros/arm_control.hpp
@@ -10,7 +10,14 @@ class ArmControl
 public:
     ArmControl();
 private:
+    void arm_control(
+        const std::shared_ptr<rere_daisha_msgs::srv::ArmPos::Request> request,
+        std::shared_ptr<rere_daisha_msgs::srv::ArmPos::Response> response
+    );
+
     double theta;
     double r;
     double height;
+
+    rclcpp::Service<rere_daisha_msgs::srv::ArmPos>::SharedPtr arm_service_;
 };
