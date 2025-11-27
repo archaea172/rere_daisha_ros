e82cea5a6b8f5c164c02f5c6e15f4f11685d24fa
aky <akiyugata271@gmail.com>
2025-11-27 11:41:01 +0900

    add func

--- src/arm_control.cpp
+++ src/arm_control.cpp
@@ -6,7 +6,19 @@ ArmControl::ArmControl()
 
 }
 
-int main()
+
+void ArmControl::arm_control(
+    const std::shared_ptr<rere_daisha_msgs::srv::ArmPos::Request> request,
+    std::shared_ptr<rere_daisha_msgs::srv::ArmPos::Response> response
+)
 {
 
+}
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<ArmControl>());
+  rclcpp::shutdown();
+  return 0;
 }
