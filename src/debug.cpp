ef4a5872012627527bc622f99b4ee786452cd15d
aky <akiyugata271@gmail.com>
2025-11-30 13:42:33 +0900

    up

--- src/debug.cpp
+++ src/debug.cpp
@@ -85,7 +85,7 @@ int main(int argc, char *argv[])
 
     for (size_t i = 0; i < 10; i++)
     {
-        wheel_vel = {0.1, -0.1};
+        wheel_vel = {0.3, -0.3};
         txdata.data = wheel_vel;
         pub_wheel_->publish(txdata);
     }
