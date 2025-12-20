dca9243ebe98488d2e30c4f2d27cd044a733e9c2
aky <akiyugata271@gmail.com>
2025-12-20 21:12:57 +0900

    add urdf

---
+++ launch/utan_robot_description.launch.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+
+
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("utan_urdf_description"),
+                    "urdf",
+                    "utan_urdf.xacro",
+                ]
+            ),
+
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    return LaunchDescription([
+        Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        output='screen',
+    ),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[robot_description]),
+    ])
